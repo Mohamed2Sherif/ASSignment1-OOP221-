d1df0623ca3c19abf102ef3b067b2f7e3c7508bc
AhmedIbrahemAhmed <96237323+AhmedIbrahemAhmed@users.noreply.github.com>
2022-10-24 11:44:21 +0200

    Add files via upload

--- ASSignment1-OOP221-/BigDecimalInt.cpp
+++ ASSignment1-OOP221-/BigDecimalInt.cpp
@@ -2,10 +2,10 @@
 
 BigDecimalInt::BigDecimalInt(string decStr) {
 
-    regex valid("[+-]*[0-9]+") ;
+    regex valid("[+-]?[0-9]+") ;
     if(regex_match(decStr,valid)) {
         if(isdigit(decStr[0]))
-        dec_int = decStr;
+            dec_int = decStr;
         else{
             dec_int = decStr.substr(1) ;
             dec_sign = decStr[0] ;
@@ -23,7 +23,7 @@ BigDecimalInt::BigDecimalInt(string decStr) {
 BigDecimalInt::BigDecimalInt (int decInt ){
     string temp = to_string(decInt) ;
     if(isdigit(temp[0]))
-    dec_int = to_string(decInt) ;
+        dec_int = to_string(decInt) ;
     else{
         dec_int = temp.substr(1) ;
         dec_sign = temp[0] ;
@@ -48,7 +48,7 @@ BigDecimalInt BigDecimalInt:: operator+ (BigDecimalInt anotherDec){
         }
         static int carry = 0 ;
         for (int i = 0; i < length; i++) {
-            // convert and add the two numbers together 
+            // convert and add the two numbers together
             int temp = (dec_int[dec_int.length() - i - 1] - '0') +
                        (anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] - '0') + carry;
             if (temp > 9) {
@@ -65,28 +65,32 @@ BigDecimalInt BigDecimalInt:: operator+ (BigDecimalInt anotherDec){
             for (int i = diff - 1; i >= 0; i--) {
                 if(carry){
                     int num = (anotherDec.dec_int[i]-'0') + carry ;
-                    if(num>9)
-                        carry = 1 ;
+                    if(num>9) {
+                        carry = 1;
+                        num -= 10 ;
+                    }
                     else
                         carry = 0 ;
                     result.dec_int = to_string(num) + result.dec_int;
                 }
                 else
-                result.dec_int = anotherDec.dec_int[i] + result.dec_int;
+                    result.dec_int = anotherDec.dec_int[i] + result.dec_int;
             }
         }
         else
             for (int i = diff - 1; i >= 0; i--) {
-                if(carry){
-                    int num = (dec_int[i]-'0') + carry ;
-                    if(num>9)
-                        carry = 1 ;
+                if (carry) {
+                    int num = (dec_int[i] - '0') + carry;
+                    if (num > 9) {
+                        carry = 1;
+                        num -= 10 ;
+                    }
                     else
                         carry = 0 ;
                     result.dec_int = to_string(num) + result.dec_int;
                 }
                 else
-                result.dec_int = dec_int[i] + result.dec_int;
+                    result.dec_int = dec_int[i] + result.dec_int;
             }
         if(carry) {
             result.dec_int = to_string(carry) + result.dec_int;
@@ -102,10 +106,10 @@ BigDecimalInt BigDecimalInt:: operator+ (BigDecimalInt anotherDec){
             result.dec_sign = '-' ;
         else{
             if(anotherDec.dec_int>dec_int) {
-              result.dec_sign = '+' ;
+                result.dec_sign = '+' ;
             }
             else if(anotherDec.dec_int<dec_int){
-              result.dec_sign = '-' ;
+                result.dec_sign = '-' ;
             }
             else{
                 result.dec_sign = ' ' ;
@@ -140,71 +144,69 @@ BigDecimalInt BigDecimalInt:: operator+ (BigDecimalInt anotherDec){
 
 //num2 - num1
 string subtract(string num1, string num2){
-        static int carry = 0 ;
-        int temp ;
-        string res = "0" ;
-        int length, diff=0 ;
-        bool check_bigger ;
+    static int carry = 0 ;
+    int temp ;
+    string res = "0" ;
+    int length, diff=0 ;
+    bool check_bigger ;
 
-        if(num1.length()==num2.length())
-        {
-            if(num2.compare(num1)>0) {
-                for(int i=0;i<num1.length();i++){
-                        temp = (num2[num2.length()-i-1]-'0') - (num1[num1.length()-i-1]-'0')- carry ;
-                        if(temp<0){
-                            temp+= 10 ;
-                            carry = 1 ;
-                        }
-                        else{
-                            carry = 0 ;
-                        }
-                        res += to_string(temp) ;
-                        carry = 0 ;
+    if(num1.length()==num2.length())
+    {
+        if(num2.compare(num1)>0) {
+            for(int i=0;i<num1.length();i++){
+                temp = (num2[num2.length()-i-1]-'0') - (num1[num1.length()-i-1]-'0') - carry ;
+                if(temp<0){
+                    temp+= 10 ;
+                    carry = 1 ;
                 }
-            }
-            else if(num2.compare(num1)==0){
-                res += "0" ;
-            }
-            else {
-                swap(num1,num2) ;
-                for(int i=0;i<num1.length();i++){
-
-                        temp = (num2[num2.length()-i-1]-'0') - (num1[num1.length()-i-1]-'0')- carry ;
-                        if(temp<0){
-                            temp += 10 ;
-                            carry = 1 ;
-                        }
-                        else{
-                            carry = 0 ;
-                        }
-                        res += to_string(temp) ;
-                        carry = 0 ;
+                else{
+                    carry = 0 ;
                 }
+                res += to_string(temp) ;
             }
         }
+        else if(num2.compare(num1)==0){
+            res += "0" ;
+        }
         else {
+            swap(num1,num2) ;
+            for(int i=0;i<num1.length();i++){
 
-            if (num1.length() > num2.length()) {
-                length = num2.length();
-                diff = num1.length() - length;
-                check_bigger = true;
-                for (int i = 0; i < num2.length(); i++) {
-
-                    temp = (num1[num1.length() - i - 1] - '0') - (num2[num2.length() - i - 1] - '0') - carry;
-                    if (temp < 0) {
-                        temp += 10;
-                        carry = 1;
-                    } else {
-                        carry = 0;
-                    }
-                    res += to_string(temp);
+                temp = (num2[num2.length()-i-1]-'0') - (num1[num1.length()-i-1]-'0')- carry ;
+                if(temp<0){
+                    temp += 10 ;
+                    carry = 1 ;
                 }
-
+                else{
+                    carry = 0 ;
+                }
+                res += to_string(temp) ;
             }
-            else {
-                length = num1.length();
-                diff = num2.length() - length;
-                check_bigger = false;
+        }
+    }
+    else {
+
+        if (num1.length() > num2.length()) {
+            length = num2.length();
+            diff = num1.length() - length;
+            check_bigger = true;
+            for (int i = 0; i < num2.length(); i++) {
+
+                temp = (num1[num1.length() - i - 1] - '0') - (num2[num2.length() - i - 1] - '0') - carry;
+                if (temp < 0) {
+                    temp += 10;
+                    carry = 1;
+                } else {
+                    carry = 0;
+                }
+                res += to_string(temp);
+            }
+
+        }
+        else {
+            length = num1.length();
+            diff = num2.length() - length;
+            check_bigger = false;
 
             for (int i = 0; i < num1.length(); i++) {
 
@@ -217,39 +219,44 @@ string subtract(string num1, string num2){
                 }
                 res += to_string(temp);
             }
-            }
-            if (check_bigger) {
-                for (int i = diff - 1; i >= 0; i--) {
-                    if(carry){
-                        int num = (num1[i]-'0') - carry ;
-                        if(num<0)
-                            carry = 1 ;
-                        else
-                            carry = 0 ;
-                        res += to_string(num) ;
-                    }
-                    else
-                        res += num1[i] ;
-                }
-            }
-            else
-                for (int i = diff - 1; i >= 0; i--) {
-                    if(carry){
-                        int num = (num2[i]-'0') - carry ;
-                        if(num<0)
-                            carry = 1 ;
-                        else
-                            carry = 0 ;
-                        res += to_string(num) ;
-                    }
-                    else
-                        res += num2[i] ;
-                }
-            if(carry) {
-                res += to_string(carry) ;
-            }
-
         }
+        if (check_bigger) {
+            for (int i = diff - 1; i >= 0; i--) {
+                if(carry){
+                    int num = (num1[i]-'0') - carry ;
+                    if(num<0) {
+                        carry = 1;
+                        num+= 10 ;
+                    }
+                    else
+                        carry = 0 ;
+                    res += to_string(num) ;
+                }
+                else
+                    res += num1[i] ;
+            }
+        }
+        else
+            for (int i = diff - 1; i >= 0; i--) {
+                if(carry) {
+                    int num = (num2[i] - '0') - carry;
+                    if (num < 0) {
+                        carry = 1;
+                        num += 10 ;
+                    }
+                    else
+                        carry = 0 ;
+                    res += to_string(num) ;
+                }
+                else
+                    res += num2[i] ;
+            }
+        if(carry) {
+            res += to_string(carry) ;
+        }
+
+    }
+    res = res.substr(1) ;
     return res ;
 }
 
@@ -314,56 +321,56 @@ BigDecimalInt BigDecimalInt::operator- (BigDecimalInt anotherDec)
     BigDecimalInt result("0");
     int length, diff=0 ;
     bool check_bigger ;
-   if(dec_sign == '+' && anotherDec.dec_sign == '+')
-   {
-       result.dec_sign =result.dec_sign;
-   }
-   else if(dec_sign == '-' && anotherDec.dec_sign == '+')
-   {
+    if(dec_sign == '+' && anotherDec.dec_sign == '+')
+    {
+        result.dec_sign =result.dec_sign;
+    }
+    else if(dec_sign == '-' && anotherDec.dec_sign == '+')
+    {
 
-       //ADD with negative sign
-       operation ="ADDWNSIGN";
-       //opearation is adition but result in negative sign
-   }
-   else if(dec_sign == '+' && anotherDec.dec_sign == '-')
-   {
+        //ADD with negative sign
+        operation ="ADDWNSIGN";
+        //opearation is adition but result in negative sign
+    }
+    else if(dec_sign == '+' && anotherDec.dec_sign == '-')
+    {
 
-       operation ="ADD";
-   }
-   else if(dec_sign == '-' && anotherDec.dec_sign == '-' )
-   {
-       if(dec_int.compare(anotherDec.dec_int)>0)
-       {
-           result.dec_sign ='-';
-       }
-       else
-       {
-           result.dec_sign = '+';
-       }
+        operation ="ADD";
+    }
+    else if(dec_sign == '-' && anotherDec.dec_sign == '-' )
+    {
+        if(dec_int.compare(anotherDec.dec_int)>0)
+        {
+            result.dec_sign ='-';
+        }
+        else
+        {
+            result.dec_sign = '+';
+        }
 
 
         result =this->operator+(anotherDec.dec_int);
-       result.dec_int.pop_back();
-       return result;
+//        result.dec_int.pop_back();
+        return result;
 
-   }
-   else {
-       result.dec_sign = '+';
+    }
+    else {
+        result.dec_sign = '+';
 
-   }
-   if (operation == "ADDWNSIGN")
-   {
-       this->dec_sign ='+';
-       anotherDec.dec_sign= '+';
-       result = this->operator+(anotherDec);
-       result.dec_sign = '-';
-       return result;
-   }
-   else if(operation == "ADD")
-   {
-       anotherDec.dec_sign ='+';
-       return this->operator+(anotherDec);
-   }
+    }
+    if (operation == "ADDWNSIGN")
+    {
+        this->dec_sign ='+';
+        anotherDec.dec_sign= '+';
+        result = this->operator+(anotherDec);
+        result.dec_sign = '-';
+        return result;
+    }
+    else if(operation == "ADD")
+    {
+        anotherDec.dec_sign ='+';
+        return this->operator+(anotherDec);
+    }
     if (anotherDec.dec_int.length() > dec_int.length()) {
 
         length = dec_int.length();
@@ -372,18 +379,18 @@ BigDecimalInt BigDecimalInt::operator- (BigDecimalInt anotherDec)
     }
     else if(anotherDec.dec_int.length() == dec_int.length())
     {
-       if (anotherDec.dec_int.compare(dec_int) >0)
-       {
-           length = dec_int.length();
-           diff = anotherDec.dec_int.length() - length;
-           check_bigger = false;
-       }
-       else
-       {
-           length = anotherDec.dec_int.length();
-           diff = dec_int.length() - length;
-           check_bigger = true ;
-       }
+        if (anotherDec.dec_int.compare(dec_int) >0)
+        {
+            length = dec_int.length();
+            diff = anotherDec.dec_int.length() - length;
+            check_bigger = false;
+        }
+        else
+        {
+            length = anotherDec.dec_int.length();
+            diff = dec_int.length() - length;
+            check_bigger = true ;
+        }
     }
 
     else {
@@ -391,62 +398,62 @@ BigDecimalInt BigDecimalInt::operator- (BigDecimalInt anotherDec)
         diff = dec_int.length() - length;
         check_bigger = true ;
     }
-   if(check_bigger)
-   {
+    if(check_bigger)
+    {
 
-       result.dec_sign ='+';
-       for(int i =0; i <length; i++)
-       {
-           if(carry == -1 && dec_int[dec_int.length() - i - 1] != '0')
-           {
-               dec_int[dec_int.length() - i - 1] -=1;
-               carry =0;
-           }
-           else if(carry == -1 && dec_int[dec_int.length() - i - 1] =='0')
-           {
-               dec_int[dec_int.length() - i - 1] +=9;
-               carry= -1;
-           }
+        result.dec_sign ='+';
+        for(int i =0; i <length; i++)
+        {
+            if(carry == -1 && dec_int[dec_int.length() - i - 1] != '0')
+            {
+                dec_int[dec_int.length() - i - 1] -=1;
+                carry =0;
+            }
+            else if(carry == -1 && dec_int[dec_int.length() - i - 1] =='0')
+            {
+                dec_int[dec_int.length() - i - 1] +=9;
+                carry= -1;
+            }
 
-           if((dec_int[dec_int.length() - i - 1] ) <( anotherDec.dec_int[anotherDec.dec_int.length() - i - 1]))
-           {
-               //make sure the carry number is not
+            if((dec_int[dec_int.length() - i - 1] ) <( anotherDec.dec_int[anotherDec.dec_int.length() - i - 1]))
+            {
+                //make sure the carry number is not
 
 
-               int temp = (dec_int[dec_int.length() - i - 1] - '0') + 10 - (anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] - '0');
-              string temps = to_string(temp);
-               result.dec_int = temps + result.dec_int;
-               carry = -1;
-           }
-           else
-           {
-               int temp = (dec_int[dec_int.length() - i - 1] - '0')  - (anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] - '0');
-               string temps =to_string(temp);
+                int temp = (dec_int[dec_int.length() - i - 1] - '0') + 10 - (anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] - '0');
+                string temps = to_string(temp);
                 result.dec_int = temps + result.dec_int;
-           }
-       }
+                carry = -1;
+            }
+            else
+            {
+                int temp = (dec_int[dec_int.length() - i - 1] - '0')  - (anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] - '0');
+                string temps =to_string(temp);
+                result.dec_int = temps + result.dec_int;
+            }
+        }
 
 
 
-       for(int  i=0 ; i <diff; i++)
-       {
-           if (carry == -1 && dec_int[diff-i-1] !='0')
-           {
-               carry =0;
-               dec_int[diff-i-1] -=1;
-           }
-           else if (carry == -1 && dec_int[diff-i-1] =='0')
-           {
-               carry=-1;
-               dec_int[diff-i-1] +=9;
-           }
-           char temps = (dec_int[diff-i-1]);
-           result.dec_int = temps + result.dec_int;
-       }
+        for(int  i=0 ; i <diff; i++)
+        {
+            if (carry == -1 && dec_int[diff-i-1] !='0')
+            {
+                carry =0;
+                dec_int[diff-i-1] -=1;
+            }
+            else if (carry == -1 && dec_int[diff-i-1] =='0')
+            {
+                carry=-1;
+                dec_int[diff-i-1] +=9;
+            }
+            char temps = (dec_int[diff-i-1]);
+            result.dec_int = temps + result.dec_int;
+        }
 
 
 
-   }
+    }
 //   // if the second number is bigger
     else
     {
@@ -457,7 +464,7 @@ BigDecimalInt BigDecimalInt::operator- (BigDecimalInt anotherDec)
         {
             if(carry == -1 && anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] !='0')
             {
-               anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] -=1;
+                anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] -=1;
                 carry =0;
             }
             else if(carry == -1 && anotherDec.dec_int[anotherDec.dec_int.length() - i - 1] =='0')
@@ -511,7 +518,6 @@ BigDecimalInt BigDecimalInt::operator- (BigDecimalInt anotherDec)
 
     result.dec_int.pop_back();
 
-   return result ;
+    return result ;
 }
-
 
